2219032c7731d41d4205dffde67da6428a918a41
284km <at284km@gmail.com>
2015-02-03 16:55:11 +0900

    bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpgradeTest::Application.config.secret_token = '98faaeb9f77e61a1eb0d59138d67fdf6eeadccdfdb01e75dd238bd97544402d34ada1bbcd5b99c6e378d7514adea003aa8edf7042edee25f9869562fe5c3bca3'
